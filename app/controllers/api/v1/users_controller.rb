2e7f32756d13de41db92f5794c0b9f0bc52b77a7
Eric Hale <eric_hale@comcast.net>
2020-11-10 11:45:53 -0700

    Add happy and sad path tests for user registration.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,19 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    # require "pry"; binding.pry
     user = Users.new(user_params)
-    user.save
-    user.api_key
-    render json: UsersSerializer.new(user), status: :created
+
+    if user_params[:email].nil?
+      render json: {message: 'Please fill in email'}, status: :unauthorized
+    elsif user_params[:password].nil?
+      render json: {message: 'Please fill in password'}, status: :unauthorized
+    elsif user_params[:password] != user_params[:password_confirmation]
+      render json: {message: 'Passwords must match'}, status: :unauthorized
+    elsif Users.find_by(email: user_params[:email]) != nil
+      render json: {message: 'A user with that email already exists'}, status: :unauthorized
+    else user.save
+      user.api_key
+      render json: UsersSerializer.new(user), status: :created
+    end
   end
 
   private
--- spec/requests/api/v1/user_registration_spec.rb
+++ spec/requests/api/v1/user_registration_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'User endpoint' do
+RSpec.describe 'Users endpoint' do
   it 'can register user and generate an API key' do
     user_params = ({ email: 'tony@stark.com',
                     password: 'kewlest@venger',
@@ -9,12 +9,13 @@ RSpec.describe 'User endpoint' do
 
 
     post '/api/v1/users', headers: headers, params: JSON.generate(user_params)
-    # require "pry"; binding.pry
     json = JSON.parse(response.body, symbolize_names: true)
 
     user = Users.last
 
-    # expect(response.status).to eq(201)
+    expect(response).to be_successful
+    expect(response.status).to eq(201)
+
     expect(json).to be_a(Hash)
     expect(json).to have_key(:data)
     expect(json[:data]).to be_a(Hash)
@@ -29,5 +30,67 @@ RSpec.describe 'User endpoint' do
     expect(json[:data][:attributes]).to have_key(:api_key)
     expect(json[:data][:attributes][:api_key]).to eq(user.api_key)
     expect(json[:data][:attributes]).to_not have_key(:password)
+  end
+
+  it 'should not register a user with no email' do
+    user_params = ({ password: 'kewlest@venger',
+                    password_confirmation: 'kewlest@venger' })
+    headers = {'CONTENT_TYPE' => 'application/json'}
+
+    post '/api/v1/users', headers: headers, params: JSON.generate(user_params)
+    json = JSON.parse(response.body, symbolize_names: true)
+
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(401)
+    expect(json).to have_key(:message)
+    expect(json[:message]).to eq('Please fill in email')
+  end
+
+  it 'should not register a user with no password' do
+    user_params = ({ email: 'tony@stark.com' })
+    headers = {'CONTENT_TYPE' => 'application/json'}
+
+    post '/api/v1/users', headers: headers, params: JSON.generate(user_params)
+    json = JSON.parse(response.body, symbolize_names: true)
+
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(401)
+    expect(json).to have_key(:message)
+    expect(json[:message]).to eq('Please fill in password')
+  end
+
+  it 'should not register a user with passwords that do not match' do
+    user_params = ({ email: 'tony@stark.com',
+                    password: 'kewlest@venger',
+                    password_confirmation: 'coolest@venger' })
+    headers = {'CONTENT_TYPE' => 'application/json'}
+
+    post '/api/v1/users', headers: headers, params: JSON.generate(user_params)
+    json = JSON.parse(response.body, symbolize_names: true)
+
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(401)
+    expect(json).to have_key(:message)
+    expect(json[:message]).to eq('Passwords must match')
+  end
+
+  it 'should not register if that user already has an account with the given email' do
+    user_params = ({ email: 'tony@stark.com',
+                    password: 'kewlest@venger',
+                    password_confirmation: 'kewlest@venger' })
+    headers = {'CONTENT_TYPE' => 'application/json'}
+
+    post '/api/v1/users', headers: headers, params: JSON.generate(user_params)
+    post '/api/v1/users', headers: headers, params: JSON.generate(user_params)
+    json = JSON.parse(response.body, symbolize_names: true)
+
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(401)
+    expect(json).to have_key(:message)
+    expect(json[:message]).to eq('A user with that email already exists')
   end
 end
