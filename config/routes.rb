d28f748d4bbe94983af4cf2731292782eaf3ad5c
Eric Hale <eric_hale@comcast.net>
2020-11-10 12:07:20 -0700

    Add test and controller for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       get 'forecast', to: 'forecast#show'
       get 'backgrounds', to: 'backgrounds#show'
       post 'users', to: 'users#create'
+      post 'sessions', to: 'sessions#create'
     end
   end
 end
---
+++ spec/requests/api/v1/user_login_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe 'Users login endpoint' do
+  it 'can login a user and return that user api key' do
+    user_params = ({ email: 'tony@stark.com',
+                    password: 'kewlest@venger',
+                    password_confirmation: 'kewlest@venger' })
+    headers = {'CONTENT_TYPE' => 'application/json'}
+    post '/api/v1/users', headers: headers, params: JSON.generate(user_params)
+
+    user_login_params = ({ email: user_params[:email],
+                         password: user_params[:password]
+                      })
+
+    post '/api/v1/sessions', headers: headers, params: JSON.generate(user_login_params)
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    user= Users.last
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+
+    expect(json).to be_a(Hash)
+    expect(json).to have_key(:data)
+    expect(json[:data]).to have_key(:type)
+    expect(json[:data][:type]).to eq('users')
+    expect(json[:data]).to have_key(:id)
+    expect(json[:data][:id]).to be_a(String)
+    expect(json[:data]).to have_key(:attributes)
+    expect(json[:data][:attributes]).to be_a(Hash)
+    expect(json[:data][:attributes]).to have_key(:email)
+    expect(json[:data][:attributes][:email]).to eq(user.email)
+    expect(json[:data][:attributes]).to have_key(:api_key)
+    expect(json[:data][:attributes][:api_key]).to eq(user.api_key)
+  end
+end
